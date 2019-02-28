ae1b206d19aa3c9fa637831f17857af0d107733f
Hernandez9029 <47732464+Hernandez9029@users.noreply.github.com>
2019-02-27 22:08:37 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,220 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Practica_8
+namespace Sobrecarga
 {
-    class FraccionDiv
+    class Dados
     {
-        //numerador
-        private int numdiv;
-        //denominador
-        private int dendiv;
+        private string color;
+        private int valor;
 
         //Constructor
-        public FraccionDiv(int numdiv, int dendiv)
+        public Dados(string color)
         {
-            this.numdiv = numdiv; this.dendiv = dendiv;
+            this.color = color;
         }
 
-        public FraccionDiv()
+        public Dados (int valor)
         {
-            numdiv = 1;
-            dendiv = 1;
+            Random aleatorio = new Random(1,6);
+            this.valor = aleatorio;
         }
 
-        //Haciendo las operaciones para multiplicación de fracciones
-        public static FraccionDiv operator /(FraccionDiv a, FraccionDiv b)
-        {
-            FraccionDiv result = new FraccionDiv(a.numdiv * b.dendiv, a.dendiv * b.numdiv);
-            return result;
-        }
-
-        //Imprime
         public void imprime()
         {
-            Console.WriteLine("{0}/{1}", numdiv, dendiv);
-        }
-
-        //Regresa valor decimal
-        public decimal a_decimal()
-        {
-            return numdiv / dendiv;
-        }
-    }
-
-    class FraccionMul
-    {
-        //numerador
-        private int numul;
-        //denominador
-        private int denmul;
-
-        //Constructor
-        public FraccionMul(int numul, int denmul)
-        {
-            this.numul = numul; this.denmul = denmul;
-        }
-
-        public FraccionMul()
-        {
-            numul = 1;
-            denmul = 1;
-        }
-
-        //Haciendo las operaciones para multiplicación de fracciones
-        public static FraccionMul operator *(FraccionMul a, FraccionMul b)
-        {
-            FraccionMul result = new FraccionMul(a.numul * b.numul, a.denmul * b.denmul);
-            return result;
-        }
-
-        //Imprime
-        public void imprime()
-        {
-            Console.WriteLine("{0}/{1}", numul, denmul);
-        }
-
-        //Regresa valor decimal
-        public decimal a_decimal()
-        {
-            return numul / denmul;
-        }
-    }
-
-    class FraccionResta
-    {
-        //numerador
-        private int numres;
-        //denominador
-        private int denres;
-
-        //Constructor
-        public FraccionResta(int numres, int denres)
-        {
-            this.numres = numres; this.denres = denres;
-        }
-
-        public FraccionResta()
-        {
-            numres = 1;
-            denres = 1;
-        }
-
-        //Haciendo las operaciones de la división negativa
-        public static FraccionResta operator -(FraccionResta a, FraccionResta b)
-        {
-            FraccionResta result = new FraccionResta(a.numres * b.denres - a.denres * b.numres, a.denres * b.denres);
-            return result;
+            Console.WriteLine("El dado mayor y su número de cara es: {0} {1}", color, valor);
         }
         
-        //Imprime
-        public void imprime()
+        public Dados()
         {
-            Console.WriteLine("{0}/{1}", numres, denres);
+            valor = 1;
         }
 
-        //Regresar valor decimal
-        public decimal a_decimal()
+        public static Dados operator ==(Dados a, Dados b)
         {
-            return numres / denres;
+            return new Dados(a.valor == b.valor);
         }
+                public static Dados operator >(Dados a, Dados b)
+        {
+            return new Dados(a.valor > b.valor);
+        }
+                public static Dados operator <(Dados a, Dados b)
+        {
+            return new Dados(a.valor < b.valor);
+        }
+
     }
-
-    class Fraccion
-    {
-        //numerador
-        private int num;
-        //denominador
-        private int den;
-
-        //Constructor
-        public Fraccion(int num, int den)
-        {
-            this.num = num; this.den = den;
-        }
-        
-        public Fraccion()
-        {
-            num = 1;
-            den = 1;
-        }
-
-        //Haciendo las operaciones de la división
-        public static Fraccion operator +(Fraccion x, Fraccion y)
-        {
-            Fraccion result = new Fraccion(x.num * y.den + x.den * y.num, x.den * y.den);
-            return result;
-        }
-        
-        //Imprime
-        public void imprime()
-        {
-            Console.WriteLine("{0}/{1}", num, den);
-        }
-
-        //Regresa valor decimal
-        public decimal a_decimal()
-        {
-            return num / den;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Suma de fracciones");
+            Dados dado1 = new Dados("Azul", valor);
+            Dados dado2 = new Dados("Negro", valor); 
+            Dados dado3 = new Dados("Blanco", valor);
 
-            //Creando objetos de la clase Fraccion
-            Fraccion a = new Fraccion(1,2);
-            Fraccion b = new Fraccion();
-            Fraccion c = a + b;
+            if (dado1 == dado2 && dado1 == dado3)
+            {
+                Console.WriteLine("No hay dado mayor, todas sus caras dieron el mismo número");
+            }
 
-            //Imprimiendo objetos
-            a.imprime();
-            b.imprime();
-            c.imprime();
+            else if (dado1 > dado2 && dado1 > dado3)
+            {
+                dado1.imprime();
+            }
 
-            Console.WriteLine("Resta de fracciones");
+            else if (dado1 == dado2 && dado1 > dado3)
+            {
+                dado1.imprime();
+            }
 
-            //Creando objetos de la clase FraccionResta
-            FraccionResta a2 = new FraccionResta(3, 4);
-            FraccionResta b2 = new FraccionResta();
-            FraccionResta c2 = a2 - b2;
+            else if (dado1 > dado2 && dado1 == dado3)
+            {
+                dado1.imprime();
+            }
 
-            //Imprimiendo objetos
-            a2.imprime();
-            b2.imprime();
-            c2.imprime();
+            else if (dado2 > dado1 && dado2 > dado3)
+            {
+                dado2.imprime();
+            }
 
-            Console.WriteLine("Multiplicación de fracciones");
+            else if (dado2 == dado1 && dado2 > dado3)
+            {
+                dado2.imprime();
+            }
 
-            //Creando objetos de la clase FraccionMul
-            FraccionMul a3 = new FraccionMul(4, 5);
-            FraccionMul b3 = new FraccionMul(3, 7);
-            FraccionMul c3 = a3 * b3;
+            else if (dado2 > dado1 && dado2 == dado3)
+            {
+                dado2.imprime();
+            }
 
-            //Imprimiendo objetos
-            a3.imprime();
-            b3.imprime();
-            c3.imprime();
+            else if (dado3 > dado1 && dado3 > dado2)
+            {
+                dado3.imprime();
+            }
 
-            Console.WriteLine("División de fracciones");
+            else if (dado3 == dado1 && dado3 > dado2)
+            {
+                dado3.imprime();
+            }
 
-            //Creando objetos de la clase FraccionDiv
-            FraccionDiv a4 = new FraccionDiv(2, 6);
-            FraccionDiv b4 = new FraccionDiv(4, 9);
-            FraccionDiv c4 = a4 / b4;
-
-            //Imprimiendo objetos
-            a4.imprime();
-            b4.imprime();
-            c4.imprime();
-
-            Console.ReadKey();
+            else if (dado3 > dado1 && dado3 == dado2)
+            {
+                dado3.imprime();
+            }
         }
     }
 }
