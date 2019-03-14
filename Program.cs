a30965d28c2ad251cbec389cd97b6d317be3d1e9
Hernandez9029 <47732464+Hernandez9029@users.noreply.github.com>
2019-03-13 21:26:01 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,105 +1,134 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Sobrecarga
+namespace Figuras
 {
-    class Dados
+    class Vector2D
     {
-        private string color;
-        private int valor;
-
-        //Constructor
-        public Dados(string color)
+        public int x, y;
+        public Vector2D(int x, int y)
         {
-            this.color = color;
+            this.x=x; this.y=y;
         }
-
-        public Dados (int valor)
+        public override string ToString()
         {
-            Random aleatorio = new Random(1,6);
-            this.valor = aleatorio;
+            return String.Format ("{0}, {1}", x, y);
         }
+    }
+    class Color
+    {
+        public string fill;
+        public string border;
 
-        public void imprime()
+        public Color(string fill, string border)
         {
-            Console.WriteLine("El dado mayor y su número de cara es: {0} {1}", color, valor);
-        }
-        
-        public Dados()
-        {
-            valor = 1;
+            this.fill = fill; this.border = border;
         }
 
-        public static Dados operator ==(Dados a, Dados b)
+        public override string ToString ()
         {
-            return new Dados(a.valor == b.valor);
+            return String.Format("{0}, {1}", fill, border);
         }
-                public static Dados operator >(Dados a, Dados b)
+    }
+    abstract class Figura
+    {
+        public Vector2D position;
+        public Color fill, border;
+        public Figura():this(new Vector2D(100,100))
         {
-            return new Dados(a.valor > b.valor);
+            
         }
-                public static Dados operator <(Dados a, Dados b)
+        public Figura(Vector2D pos)
         {
-            return new Dados(a.valor < b.valor);
+            position = pos;
         }
 
+        public Figura():this(new Color("white", "black"))
+        {
+
+        }
+        public Figura(Color fill, Color border)
+        {
+            this.fill = fill; this.border = border;
+        }
+
+        public abstract void Dibuja();
+    }
+    
+    class Circulo : Figura
+    {
+        private int radio;
+        public Circulo(Vector2D pos, int radio):base(pos)
+        {
+            this.radio = radio;
+        }
+        public Circulo(Color fi, Color bor):base(fi, bor)
+        {
+            
+        }
+        public Circulo():base()
+        {
+            this.radio = 10;
+            this.fill = "white";
+            this.border = "black";
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibuja un circulo en {0} de color {1}", position, fill);
+        }
+    }
+    
+    class Rectangulo : Figura
+    {
+        public Rectangulo(Vector2D pos):base(pos)
+        {
+            
+        }
+        public Rectangulo(Color fi, Color bor):base(fi, bor)
+        {
+            
+        }
+        public Rectangulo():base()
+        {
+            this.fill = "blue";
+            this.border = "black";
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibuja un rectangulo en {0} de color {1}", position, fill);
+        }
+    }
+
+    class Cuadrado : Figura
+    {
+        public Cuadrado(Vector2D pos):base(pos)
+        {
+
+        }
+        public Cuadrado(Color fi, Color bor):base(fi, bor)
+        {
+            
+        }
+        public Cuadrado():base()
+        {
+            this.fill = "red";
+            this.border = "black";
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibuja un cuadrado en {0} de color {1}", position, fill);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Dados dado1 = new Dados("Azul", valor);
-            Dados dado2 = new Dados("Negro", valor); 
-            Dados dado3 = new Dados("Blanco", valor);
-
-            if (dado1 == dado2 && dado1 == dado3)
-            {
-                Console.WriteLine("No hay dado mayor, todas sus caras dieron el mismo número");
-            }
-
-            else if (dado1 > dado2 && dado1 > dado3)
-            {
-                dado1.imprime();
-            }
-
-            else if (dado1 == dado2 && dado1 > dado3)
-            {
-                dado1.imprime();
-            }
-
-            else if (dado1 > dado2 && dado1 == dado3)
-            {
-                dado1.imprime();
-            }
-
-            else if (dado2 > dado1 && dado2 > dado3)
-            {
-                dado2.imprime();
-            }
-
-            else if (dado2 == dado1 && dado2 > dado3)
-            {
-                dado2.imprime();
-            }
-
-            else if (dado2 > dado1 && dado2 == dado3)
-            {
-                dado2.imprime();
-            }
-
-            else if (dado3 > dado1 && dado3 > dado2)
-            {
-                dado3.imprime();
-            }
-
-            else if (dado3 == dado1 && dado3 > dado2)
-            {
-                dado3.imprime();
-            }
-
-            else if (dado3 > dado1 && dado3 == dado2)
-            {
-                dado3.imprime();
-            }
+            List<Figura> figuras = new List<Figura>();
+            figuras.Add(new Circulo());
+            figuras.Add(new Rectangulo(new Vector2D(200,200) ));
+            figuras.Add(new Cuadrado(new Vector2D(300,300) ));
+            foreach(Figura f in figuras)
+                f.Dibuja();
         }
     }
 }
